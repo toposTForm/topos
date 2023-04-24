9879bbfda33e72db167de1a378464c6ba1ac2979
Romanuch <bereslavskiy0@gmail.com>
2023-04-24 19:25:55 +0300

    graph

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,6 @@ import Cell from "@/components/Cell/Cell";
 import MyQuillEditor from "@/components/QuillEditor/MyQuillEditor";
 import {ButtonPlugin} from "@syncfusion/ej2-vue-buttons";
 import naive from "naive-ui";
-import GolosText_Regular from '@/fonts/Golos Text/Golos-Text_Regular.ttf';
-import GolosText_Bold from '@/fonts/Golos Text/Golos-Text_Bold.ttf';
 
 export default {
   name: 'App',
@@ -39,13 +37,11 @@ export default {
   font-style: normal;
   font-family: 'Golos-Text_Regular';
   src: url("./fonts/Golos Text/Golos-Text_Regular.ttf") format("truetype");;
-  src: local('GolosText_Regular');
 }
 @font-face{
   font-display: auto;
   font-style: normal;
   font-family: 'Golos Text Bold';
   src: url("./fonts/Golos Text/Golos-Text_Bold.ttf") format("truetype");;
-  src: local('GolosText_Bold') ;
 }
 </style>
