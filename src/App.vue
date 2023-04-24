933bc570e488edb932b75da921a2c33419dde385
Romanuch <bereslavskiy0@gmail.com>
2023-04-24 18:35:40 +0300

    graph

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,14 @@
 <script>
 /* eslint-disable */
 import store from './store'
-import RightClickMenu from "@/components/RightClickMenu/RightClickMenu";
 import FormList2 from "@/components/FormList/FormList2";
 import CellList from "@/components/CellList/CellList";
 import Cell from "@/components/Cell/Cell";
 import MyQuillEditor from "@/components/QuillEditor/MyQuillEditor";
 import {ButtonPlugin} from "@syncfusion/ej2-vue-buttons";
 import naive from "naive-ui";
-
+import GolosText_Regular from '@/fonts/Golos Text/Golos-Text_Regular.ttf';
+import GolosText_Bold from '@/fonts/Golos Text/Golos-Text_Bold.ttf';
 
 export default {
   name: 'App',
@@ -24,7 +24,9 @@ export default {
     CellList,
     Cell,
     MyQuillEditor,
-    naive
+    naive,
+    GolosText_Regular,
+    GolosText_Bold
   },
   methods: {
   }
@@ -33,16 +35,19 @@ export default {
 </script>
 
 <style>
+
 @font-face{
   font-display: auto;
   font-style: normal;
   font-family: 'Golos-Text_Regular';
-  src: url("./fonts/Golos Text/Golos-Text_Regular.ttf");
+  /*src: url("./fonts/Golos Text/Golos-Text_Regular.ttf");*/
+  src: local('GolosText_Regular');
 }
 @font-face{
   font-display: auto;
   font-style: normal;
   font-family: 'Golos Text Bold';
-  src: url("./fonts/Golos Text/Golos-Text_Bold.ttf");
+  /*src: url("./fonts/Golos Text/Golos-Text_Bold.ttf");*/
+  src: local('GolosText_Bold');
 }
 </style>
